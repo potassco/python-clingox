0073cbcad93079707407177f0df7cb3aa4a3653b
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-09-01 10:41:24 +0200

    test the transformer

--- clingox/tests/test_ast.py
+++ clingox/tests/test_ast.py
@@ -7,7 +7,7 @@ from typing import List
 
 from clingo import parse_program
 from clingo.ast import AST
-from ..ast import Visitor
+from ..ast import Visitor, Transformer
 
 
 class TestVisitor(Visitor):
@@ -61,6 +61,42 @@ def test_visit(s: str) -> str:
     return v.result
 
 
+class TestTransformer(Transformer):
+    '''
+    Simple transformer renaming variables.
+    '''
+    # pylint: disable=invalid-name
+    result: str
+
+    def __init__(self):
+        self.result = ""
+
+    def visit_Program(self, x: AST, suffix: str):
+        '''
+        Remove program parts.
+        '''
+        return None
+
+    def visit_Variable(self, x: AST, suffix: str):
+        '''
+        Add suffix to variable.
+        '''
+        x.name = x.name + suffix
+        return x
+
+def test_transform(s: str) -> str:
+    '''
+    Test the transformer by parsing the given program and using the
+    TestTransformer on it.
+    '''
+    prg: List[AST]
+    prg = []
+    parse_program(s, prg.append)
+    v = TestTransformer()
+    ret = v.visit_list(prg, "_x")
+    return "\n".join(str(x) for x in ret if x is not None)
+
+
 class TestAST(TestCase):
     '''
     Tests for AST manipulation.
@@ -71,3 +107,9 @@ class TestAST(TestCase):
         Test the visitor.
         '''
         self.assertEqual(test_visit("a(X) :- p(X)."), "rlavlav")
+
+    def test_transform(self):
+        '''
+        Test the transformer.
+        '''
+        self.assertEqual(test_transform("a(X) :- p(X)."), "a(X_x) :- p(X_x).")
