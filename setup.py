c18d892cdfb75b765017a85da907396f1025af72
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-02-10 17:48:30 +0100

    update pip package metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
+from textwrap import dedent
 from setuptools import setup
-
 
 setup(
     version='1.0.0',
     name='clingox',
     description='Support library for clingo.',
+    long_description=dedent('''\
+        Preliminary documentation can be found here:
+
+        - https://www.cs.uni-potsdam.de/~kaminski/pyclingo-cffi/
+        '''),
+    long_description_content_type='text/markdown',
     author='Roland Kaminski',
+    author_email='kaminski@cs.uni-potsdam.de',
     license='MIT',
+    url='https://github.com/potassco/python-clingox',
+    install_requires=['clingo-cffi'],
     packages=['clingox'],
     zip_safe=False,
+    python_requires=">=3.6"
 )
