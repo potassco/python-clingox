3ccd1831dfac9f00ae15031933dcc985dcad39cd
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-04-08 19:19:10 +0200

    update deploy scripts

--- .github/pipsource.py
+++ .github/pipsource.py
@@ -2,38 +2,54 @@
 Script to build pip source package.
 '''
 
-from re import finditer, escape, match, sub
+import argparse
+from re import finditer, escape, match, sub, search
 from subprocess import check_call, check_output
 
+from rename import rename_clingo_cffi
 
-def adjust_version():
+def adjust_version(url):
     '''
     Adjust version in setup.py.
     '''
-    pip = check_output(['curl', '-sL', 'https://test.pypi.org/simple/clingox']).decode()
-    version = '1.0.0'
+    with open('setup.py') as fr:
+        setup = fr.read()
 
+    package_name = search(r'''name[ ]*=[ ]*['"]([^'"]*)['"]''', setup).group(1)
+    package_regex = package_name.replace('-', '[-_]')
+
+    pip = check_output(['curl', '-sL', '{}/{}'.format(url, package_name)]).decode()
+    version = '1.0.0'
     assert version is not None
 
     post = 0
-    for m in finditer(r'clingox-{}\.tar\.gz'.format(escape(version)), pip):
+    for m in finditer(r'{}-{}\.tar\.gz'.format(package_regex, escape(version)), pip):
         post = max(post, 1)
 
-    for m in finditer(r'clingox-{}\.post([0-9]+)\.tar\.gz'.format(escape(version)), pip):
+    for m in finditer(r'{}-{}\.post([0-9]+)\.tar\.gz'.format(package_regex, escape(version)), pip):
         post = max(post, int(m.group(1)) + 1)
 
-    for m in finditer(r'clingox-{}\.post([0-9]+).*\.whl'.format(escape(version)), pip):
+    for m in finditer(r'{}-{}\.post([0-9]+).*\.whl'.format(package_regex, escape(version)), pip):
         post = max(post, int(m.group(1)))
 
-    with open('setup.py') as fr:
-        setup = fr.read()
     with open('setup.py', 'w') as fw:
         if post > 0:
             fw.write(sub('version( *)=.*', 'version = \'{}.post{}\','.format(version, post), setup, 1))
         else:
             fw.write(sub('version( *)=.*', 'version = \'{}\','.format(version), setup, 1))
 
-if __name__ == "__main__":
-    adjust_version()
+def run():
+    parser = argparse.ArgumentParser(description='Build source package.')
+    parser.add_argument('--release', action='store_true', help='Build release package.')
+    args = parser.parse_args()
+    if args.release:
+        rename_clingo_cffi()
+        url = 'https://pypi.org/simple'
+    else:
+        url = 'https://test.pypi.org/simple'
+
+    adjust_version(url)
     check_call(['python3', 'setup.py', 'sdist', 'bdist_wheel'])
 
+if __name__ == "__main__":
+    run()
