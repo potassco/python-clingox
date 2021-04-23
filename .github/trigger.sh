73ab759ded3e195c0df089b859fb18b3b4133f02
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-04-23 19:26:18 +0200

    update trigger script

--- .github/trigger.sh
+++ .github/trigger.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-dev_branch=wip
-
 function list() {
     curl \
       -X GET \
       -H "Accept: application/vnd.github.v3+json" \
       "https://api.github.com/repos/potassco/python-clingox/actions/workflows" \
-      -d "{\"ref\":\"${dev_branch}\"}"
+      -d "{\"ref\":\"ref\"}"
 }
 
 function dispatch() {
@@ -17,25 +15,34 @@ function dispatch() {
       -X POST \
       -H "Accept: application/vnd.github.v3+json" \
       "https://api.github.com/repos/potassco/python-clingox/actions/workflows/$1/dispatches" \
-      -d "{\"ref\":\"${dev_branch}\"}"
+      -d "{\"ref\":\"$3\",\"inputs\":{\"wip\":\"$2\"${4:+,$4}}}"
 }
+
+branch=wip
+wip=true
 
 case $1 in
     list)
         list
         ;;
+    release)
+        if [[ $# < 2 ]]; then
+            echo "usage: trigger release REF"
+            exit 1
+        fi
+        wip=false
+        branch=$2
+        ;&
     dev)
         # .github/workflows/ppa-dev.yml
-        dispatch 5997696
+        dispatch 5997696 $wip $branch
         # .github/workflows/pipsource.yml
-        dispatch 6065447
+        dispatch 6065447 $wip $branch
         # .github/workflows/conda-dev.yml
-        dispatch 6066933
-        ;;
-    release)
-        echo "implement me"
+        dispatch 6066933 $wip $branch
         ;;
     *)
         echo "usage: trigger {list,dev,release}"
+        exit 1
         ;;
 esac
