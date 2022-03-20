8117649a49efc6c118498999727f1f44ce2fd53a
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2022-03-20 12:10:07 +0100

    implement a tarjan - untested!!!

--- clingox/reify.py
+++ clingox/reify.py
@@ -2,25 +2,150 @@
 Document me!!!
 '''
 
-from typing import Sequence, Tuple
+from typing import Dict, Generic, List, Sequence, Tuple, TypeVar
 
 from clingo.backend import HeuristicType, Observer, TruthValue
 from clingo.symbol import Function, Number, Symbol
 
 __all__ = ['Reifier']
 
+
+class Node:
+    def __init__(self, name, visited):
+        self.name = name
+        self.visited = visited
+        self.index = 0
+        self.edges = []
+
+T = TypeVar('T')
+
+class Graph(Generic[T]):
+    def __init__(self):
+        self._names: Dict[T, int] = {}
+        self._vertices: List[Node] = []
+        self._phase: bool = True
+
+    def _visited(self, key_u):
+        return self._vertices[key_u].visited != (not self._phase)
+
+    def _active(self, key_u):
+        return self._vertices[key_u].visited != self._phase
+
+    def _add_vertex(self, val_u: T) -> int:
+        n = len(self._vertices)
+        key_u = self._names.setdefault(val_u, n)
+        if n == key_u:
+            self._vertices.append(Node(val_u, not self._phase))
+        return key_u
+
+    def add_edge(self, val_u: T, val_v: T) -> None:
+        key_u = self._add_vertex(val_u)
+        key_v = self._add_vertex(val_v)
+        self._vertices[key_u].edges.append(key_v)
+
+    def tarjan(self) -> List[List[T]]:
+        '''
+        // Check!!!
+        SCCVec sccs;
+        NodeVec stack;
+        NodeVec trail;
+        for (auto &x : nodes_) {
+            if (x.visited_ == nphase()) {
+                unsigned index = 1;
+                auto push = [&stack, &trail, &index](Node &x) {
+                    x.visited_  = ++index;
+                    x.finished_ = x.edges_.begin();
+                    stack.emplace_back(&x);
+                    trail.emplace_back(&x);
+                };
+                push(x);
+                while (!stack.empty()) {
+                    auto &y = stack.back();
+                    auto end = y->edges_.end();
+                    for (; y->finished_ != end && (*y->finished_)->visited_ != nphase(); ++y->finished_) { }
+                    if (y->finished_ != end) { push(**y->finished_++); }
+                    else {
+                        stack.pop_back();
+                        bool root = true;
+                        for (auto &z : y->edges_) {
+                            if (z->visited_ != phase_ && z->visited_ < y->visited_) {
+                                root = false;
+                                y->visited_ = z->visited_;
+                            }
+                        }
+                        if (root) {
+                            sccs.emplace_back();
+                            do {
+                                sccs.back().emplace_back(trail.back());
+                                trail.back()->visited_ = phase_;
+                                trail.pop_back();
+                            }
+                            while (sccs.back().back() != y);
+                        }
+                    }
+                }
+            }
+        }
+        phase_ = nphase();
+        return sccs;
+        '''
+        sccs: List[List[T]] = []
+        stack = []
+        trail = []
+        index = 1
+        def push(key_u):
+            nonlocal index
+            index += 1
+            stack.append(key_u)
+            trail.append(key_u)
+
+        for key_u in range(len(self._vertices)):
+            if not self._visited(key_u):
+                continue
+            index = 1
+            push(key_u)
+            while stack:
+                key_v = stack[-1]
+                vtx_v = self._vertices[key_v]
+                len_v = len(vtx_v.edges)
+                while vtx_v.index < len_v:
+                    if self._vertices[vtx_v.index].visited == (not self._phase):
+                        push(vtx_v.edges[vtx_v.index])
+                        vtx_v.index += 1
+                        break
+                else:
+                    stack.pop()
+                    root = True
+                    for key_w in vtx_v.edges:
+                        vtx_w = self._vertices[key_w]
+                        if self._active(key_w) and vtx_w.visited < vtx_v.visited:
+                            root = False
+                            vtx_v.visited = vtx_w.visited
+                    if root:
+                        key_last = None
+                        sccs.append([])
+                        while key_last != key_v:
+                            key_last = trail[-1]
+                            vtx_last = self._vertices[key_last]
+                            sccs[-1].append(vtx_last.name)
+                            vtx_last.visited = self._phase
+                            trail.pop()
+
+        self._phase = not self._phase
+        return sccs
+
 class _StepData:
     def __init__(self):
         self.atom_tuples = {}
         self.lit_tuples = {}
         self.wlit_tuples = {}
+        self.graph = Graph()
 
 def _lit(i, lit):
     return [i, Number(lit)]
 
 def _wlit(i, wlit):
     return [i, Number(wlit[0]), Number(wlit[1])]
-
 
 class Reifier(Observer):
     '''
@@ -36,13 +161,16 @@ class Reifier(Observer):
 
     def _add_edges(self, head, body):
         if self._calculate_sccs:
-            raise RuntimeError("impplement me!!!")
+            for u in head:
+                for v in body:
+                    self._step_data.graph.add_edge(u, v)
 
     def _output(self, name, args):
+        if self._reify_steps:
+            args = args + [self._step]
         self._cb(Function(name, args))
 
     def _tuple(self, name, snmap, elems, afun=_lit):
-        # TODO: refactor to account for weighted literal tuples too
         s = tuple(sorted(elems))
         n = len(snmap)
         i = Number(snmap.setdefault(s, n))
